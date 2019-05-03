1b2e3c1ac4861e5e6806379d5e8d9d59cfc6ad88
DuongNT <duongnt@tohsoft.com>
2019-05-03 18:05:44 +0700

    control-bar

---
+++ public/js/learning-page.js
@@ -0,0 +1,73 @@
+$( document ).ready(function() {
+    initializePlayerControlBar()
+    $(".lecture-title").click(function(){
+        toggleLectureList()
+    })
+    $("#btnCloseSidebar").click(()=>{
+        toggleLectureList()
+
+    })
+
+
+    var player = videojs('my-video');
+    $(".vjs-play-control").bind("click", function(){
+            if($(".vjs-play-control").hasClass("vjs-paused")){
+                $(".learning-desc-panel").fadeOut()       
+            }else{
+                $(".learning-desc-panel").fadeIn()       
+            }
+    })
+    $("video").bind("click", function(){
+        if($(".vjs-play-control").hasClass("vjs-paused")){
+            $(".learning-desc-panel").fadeIn()       
+        }else{
+            $(".learning-desc-panel").fadeOut()       
+        }
+    })
+
+    $("#lnDescBtnPlay").click(function(){
+        player.play()
+        $(".learning-desc-panel").fadeOut()
+    })
+
+
+
+    function toggleLectureList(){
+        if(!$(".learning-lecture-list").hasClass('active')){
+            
+            $(".learning-lecture-list").addClass('active')
+            $("#my-video").addClass('sidebarActivee')
+            $(".learning-desc-panel-body").addClass('sidebarActive')
+        }else{
+
+            $(".learning-lecture-list").removeClass('active')
+            $("#my-video").removeClass('sidebarActivee')
+            $(".learning-desc-panel-body").removeClass('sidebarActive')
+        }
+    }
+
+    function initializePlayerControlBar(){
+        //Time Controller Buttons
+        var speedGroup = "<div class='group-btn-control-time'></div>"
+        var btnRewind = "<button class='btn' id='btnRewind'><i class='fas fa-undo-alt'></i></button>"
+        var btnSpeed = "<button id='btnSpeed' class='btn btn-primary'>1x</button>"
+        var btnForward = "<button class='btn' id='btnRewind'><i class='fas fa-redo-alt'></i></button>"
+        
+        $(".vjs-play-control").after(speedGroup)
+        $(".group-btn-control-time").append(btnRewind)
+        $("#btnRewind").after(btnSpeed)
+        $("#btnSpeed").after(btnForward)
+
+
+        //Displaying Time
+        var groupPlayerTimeDiv = "<div class='group-player-time btn'></div>"
+        var currentTimeSpan = "<span class='player-current-time'>00:00 /</span>"
+        var endTimeSpan  = "<span class='player-end-time'> 17:09</span>"
+
+        $(".group-btn-control-time").after(groupPlayerTimeDiv)
+        $(".group-player-time").append(currentTimeSpan)
+        $(".player-current-time").after(endTimeSpan)
+        
+    }
+    
+});
--- resources/views/learning-page/index.blade.php
+++ resources/views/learning-page/index.blade.php
@@ -12,9 +12,11 @@
         <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha256-pasqAKBDmFT4eHoN2ndd6lN370kFiGUFyTiUHWhU7k8=" crossorigin="anonymous"></script>
         <!-- If you'd like to support IE8 (for Video.js versions prior to v7) -->
         <script src="https://vjs.zencdn.net/ie8/1.1.2/videojs-ie8.min.js"></script>
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+        <script type="text/javascript" src="{{ asset('js/learning-page.js') }}"></script>
+
         
 
         <!-- Styles -->
@@ -27,6 +29,8 @@
         
     </head>
     <body>
+
+        {{-- TOP BAR --}}
         <div class="learning-top">
             <div class="lecture-title">
                 <i class="fas fa-list-ul"></i>
@@ -36,6 +40,42 @@
                 <p>Go to Dashboard</p>
             </div>
         </div>
+
+        {{-- DESCRIPTION PANEL --}}
+        <div class="learning-desc-panel ">
+            <div class="learning-desc-panel-body align-items-center">
+                <div class="ln-desc-title">
+                    <h3>Unreal Engine C++ Development</h3>
+                </div>
+                <div class="ln-desc-subtitle">
+                    <h5>Section 2, Lecture 17</h5>
+                </div>
+                <div class="ln-desc-content">
+                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                </div>
+                <div class="ln-desc-achv">
+                    <p>79 of 284 items completed</p>
+                    <div class="ln-progress-bar">
+                        <div class="progress lecture-progress" style="width: 30vw">
+                            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                        </div>
+                        &nbsp;
+                        <div class="cup-progress">
+                            <div class="progress" style="width: 5vw">
+                                <div class="progress-bar bg-success" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                            </div>
+                            <i class="fas fa-trophy"></i>
+                        </div>
+                    </div>
+                </div>
+                <br>
+                <div class="ln-desc-btn-play">
+                    <button class="btn btn-warning" id="lnDescBtnPlay"><i class="fas fa-play-circle"></i> Continue</button>
+                </div>
+            </div>
+        </div>
+
+        {{-- VIDEO PLAYER --}}
         <div class="learning-video">
             <video id='my-video' class='video-js vjs-big-play-centered' controls preload='auto' data-setup='{}'>
                 <source src='http://45.56.82.249:1935/vod/_definst_/neuanhdi.mp4/playlist.m3u8' type='application/x-mpegURL'>
@@ -48,7 +88,8 @@
                 </video>
                 
         </div>
-
+        
+        {{-- LEFT SIDEBAR aka LECTURE LIST --}}
         <div class="learning-lecture-list">
             <div class="learning-lecture-list-searchbar">
                 <div class="input-group">
@@ -56,32 +97,60 @@
                     <div class="input-group-append">
                         <span class="input-group-text"><i class="fas fa-search"></i></span>
                     </div>
-                    <button class="btn float-right"><i class="fas fa-times-circle"></i></button>
+                    <button class="btn float-right" id="btnCloseSidebar"><i class="fas fa-times-circle"></i></button>
+                </div>
+            </div>
+            <div class="learning-lecture-list-body">
+                <div class="accordion" id="accordionExample">
+                    <div class="card">
+                        <div class="card-header" id="headingOne">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            Section 1: How to tackle
+                            </button>
+                        </h2>
+                        </div>
+                    
+                        <div id="collapseOne" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingTwo">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            Section 2: How to kick the ball
+                            </button>
+                        </h2>
+                        </div>
+                        <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-header" id="headingThree">
+                        <h2 class="mb-0">
+                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            Section 3: How to receive the ball
+                            </button>
+                        </h2>
+                        </div>
+                        <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
 
         <script src='https://vjs.zencdn.net/7.5.4/video.js'></script>                    
         <script src="https://cdnjs.cloudflare.com/ajax/libs/videojs-contrib-hls/5.15.0/videojs-contrib-hls.js"></script>
-        <script>
-            $( document ).ready(function() {
-                function toggleLectureList(){
-                    if($(".learning-lecture-list").width() === 0){
-                        $(".learning-lecture-list").css({'width': '35vw', 'display': 'block'})
-                        $("#my-video").css({"width": "65vw", "float": "right"})
-                        $(".learning-top").css({"width": "65vw", "left": "35vw", "transition": "0.5s"})
-                    }else{
-                        $("#my-video").css({"width": "100vw"})
-                        $(".learning-top").css({"width": "100vw", "left": "0", "transition": "0.5s"})
-                        $(".learning-lecture-list").css({'width': '0', 'display': 'none'})
-                    }
-                }
-                $(".lecture-title").click(function(){
-                    toggleLectureList()
-                })
-
-            });
-        </script>
     </body>
 </html>
         
