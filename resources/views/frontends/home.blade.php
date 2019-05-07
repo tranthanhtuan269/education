fc7646a5619b9787f2a237e6a55394f46b76ef81
tuantt <tuantt@tohsoft.com>
2019-05-07 14:56:33 +0700

    css

--- resources/views/frontends/home.blade.php
+++ resources/views/frontends/home.blade.php
@@ -2,64 +2,46 @@
 @section('content')
 <div class="banner">
     <img class="pd-mb-banner hidden-xs" src="https://cms.unica.vn/upload/images/65055147_30-4-1-5-chao-mung-dai-le-hoc-ngay-keo-tre-chi-249k-khoa_thumb.png" width="100%" alt="30/4 -1/5: Chào mừng đại lễ - Học ngay kẻo trễ [Chỉ 249k/khóa]">
-    <div class="container">
+    <!-- <div class="container">
         <div class="col-md-12">
             <div class="slick-course">
-                <div class="item text-center"  style="margin:0 8px;width: 330px" class="slick-slide">
+                @for($i = 0; $i < 10; $i++)
+                <div class="item text-center" style="margin:0 8px;width: 220px" class="slick-slide">
                     <img src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
                     <div class="cat">
-                        <h2>GYM</h2>
+                        <h2>GYM {{ $i + 1 }}</h2>
                         <p>Comming soon</p>
                     </div>
                 </div>
-                <div class="item text-center"  style="margin:0 8px;width: 330px" class="slick-slide">
-                    <img src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                    <div class="cat">
-                        <h2>GYM</h2>
-                        <p>Comming soon</p>
-                    </div>
-                </div>
-                <div class="item text-center"  style="margin:0 8px;width: 330px" class="slick-slide">
-                    <img src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                    <div class="cat">
-                        <h2>GYM</h2>
-                        <p>Comming soon</p>
-                    </div>
-                </div>
-                <div class="item text-center"  style="margin:0 8px;width: 330px" class="slick-slide">
-                    <img src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                    <div class="cat">
-                        <h2>GYM</h2>
-                        <p>Comming soon</p>
-                    </div>
-                </div>
+                @endfor
             </div>
         </div>
-    </div>
-    <script type="text/javascript">
+    </div> -->
+    <!-- <script type="text/javascript">
         $(document).ready(function(){
           $('.slick-course').slick({
             dots: false,
             infinite: true,
-            speed: 1000,
+            speed: 1500,
             slidesToShow: 1,
-            centerMode: true,
+            centerMode: false,
             variableWidth: true,
             centerPadding: '0px',
             prevArrow: false,
             nextArrow: false
           });
         });
-    </script>
+    </script> -->
 
 
-<!--                 <script type="text/javascript">
+                <script type="text/javascript">
                     config_slider_init = function() {
                         var config_options = {
                           $Loop:0,
-                          $FillMode:0,
+                          $FillMode:5,
                           $SlideWidth: 200,
                           $SlideSpacing:20,
+                          $MinDragOffsetToSlide:0,
                           $SlideDuration:1600,
                           $ArrowKeyNavigation:0,
                           $ArrowNavigatorOptions: {
@@ -103,29 +85,20 @@
                 <div id="config-slider" style="position:relative;margin:0 auto;top:0px;left:0px;width:980px;height:120px;overflow:hidden;visibility:hidden;">
      
                     <div data-u="slides" style="cursor:default;position:relative;top:0px;left:0px;width:980px;height:120px;overflow:hidden;">
-                        <div>
+                        @for($i = 0; $i < 10; $i++)
+                        <div class="item item-slider text-center">
                             <img data-u="image" src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
+                            <div class="cat cat-item-slider">
+                                <h2>GYM {{ $i + 1 }}</h2>
+                                <p>Comming soon</p>
+                            </div>
                         </div>
-                        <div>
-                            <img data-u="image" src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                        </div>
-                        <div>
-                            <img data-u="image" src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                        </div>
-                        <div>
-                            <img data-u="image" src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                        </div>
-                        <div>
-                            <img data-u="image" src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                        </div>
-                        <div>
-                            <img data-u="image" src="{{ asset('frontend/images/slide_design.png') }}" alt=""/>
-                        </div>
+                        @endfor
                     </div>
 
 
                 </div>
-                <script type="text/javascript">config_slider_init();</script> -->
+                <script type="text/javascript">config_slider_init();</script>
 </div>
 
 @include('frontends.feature-courses')
