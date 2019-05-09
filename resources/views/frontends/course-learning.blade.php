53af5dc2dbaf4f0bf690487c12cae80e8d8786fa
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-05-09 17:23:15 +0700

    css

--- resources/views/frontends/course-learning.blade.php
+++ resources/views/frontends/course-learning.blade.php
@@ -33,12 +33,22 @@
 							</div>
 							<div class="col-xs-3">
 								<div class="cup-icon">
-									<i class="fa fa-trophy fa-3x" aria-hidden="true"></i>
+									<i class="fa fa-trophy fa-2x" aria-hidden="true"></i>
 								</div>
 							</div>
 						</div>
-						<div class="progress border-element">
-							<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+						<div class="row">
+							<div class="col-xs-9">
+								<div class="progress border-element-l">
+									<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%;">
+									</div>
+								</div>
+							</div>
+							<div class="col-xs-3">
+								<div class="progress border-element-r">
+									<div class="progress-bar progress-bar-success" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width: 0;">
+									</div>
+								</div>
 							</div>
 						</div>
 					</div>
