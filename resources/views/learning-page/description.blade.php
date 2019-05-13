2be3027878519fe6a8b4cb8c37d07577f2cc300b
DuongNT <duongnt@tohsoft.com>
2019-05-13 17:39:38 +0700

    component distribution

---
+++ resources/views/learning-page/description.blade.php
@@ -0,0 +1,73 @@
+<div class="learning-desc-panel ">
+    <div class="learning-desc-panel-body align-items-center">
+        <div class="ln-desc-title">
+            <p>Unreal Engine C++ Development</p>
+        </div>
+        <div class="ln-desc-subtitle">
+            <p>Section 2, Lecture 17</p>
+        </div>
+        <div class="ln-desc-content">
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        </div>
+        <div class="ln-desc-achv">
+            <p>79 of 284 items completed</p>
+            <div class="ln-progress-bar">
+                <div class="progress lecture-progress" style="width: 30vw">
+                    <div class="progress-bar progress-bar-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+                </div>
+                &nbsp;
+                <div class="cup-progress">
+                    <div class="progress" style="width: 5vw">
+                        <div class="progress-bar bg-success" role="progressbar" style="width: 0%" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
+                    </div>
+                    <i class="fas fa-trophy"></i>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="ln-desc-btn-play">
+            <button class="btn btn-warning" id="lnDescBtnPlay"><i class="fas fa-play-circle"></i> Continue</button>
+        </div>
+    </div>
+
+    <div class="ln-desc-bottom">
+        <div class="ln-desc-btm-center">
+            <div class="ln-desc-btm-group-track">
+                <a href="2">
+                    <button class="btn" id="lnDescBtnPrevious"><i class="fas fa-step-backward"></i></button>
+                </a>
+                <a href="3">
+                    <button class="btn" id="lnDescBtnNext"><i class="fas fa-step-forward"></i></button>
+                </a>
+            </div>
+            <div class="ln-desc-group-btn-utilities">
+                <div class="btn ln-btn-server">
+                    <i class="fas fa-server"></i>
+                    <span>&nbsp;Server Video</span>
+                </div>
+                <div class="btn ln-btn-note">
+                    <i class="fas fa-sticky-note"></i>
+                    <span>&nbsp;Note</span>
+                </div>
+                <div class="btn ln-btn-discuss">
+                    <i class="fas fa-comments"></i>
+                    <span>&nbsp;Discussion</span>
+                </div>
+                <div class="btn ln-btn-file">
+                    <i class="fas fa-file-alt"></i>
+                    <span>&nbsp;Files</span>
+                </div>
+            </div>
+            <div class="ln-desc-group-btn-utilities-2">
+                <div class="btn ln-btn-autoplay">
+                    <i class="fas fa-toggle-on"></i>
+                    <span>&nbsp;Autoplay</span>
+                </div>
+                <div class="btn ln-btn-report">
+                    <i class="fas fa-exclamation-circle"></i>
+                    <span>&nbsp;Report</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/learning-page/files.blade.php
@@ -0,0 +1,43 @@
+<div class="learning-files">
+    <div class="ln-files-header">
+        <div id="btnCloseFiles"><i class="fas fa-times-circle"></i></div>
+        <p>Files</p>
+        <p></p>
+    </div>
+    <div class="ln-files-body">
+        <div class="ln-files-list">
+            <div class="ln-files-wrapper">
+                <div>
+                    <a href="">
+                        <p>
+                            <i class="fas fa-file-archive"></i>&nbsp;
+                            A google drive link
+                        </p>
+                    </a>
+                </div>
+            </div>
+            <div class="ln-files-wrapper">
+                <div>
+                    
+                    <a href="">
+                        <p>
+                            <i class="fas fa-file-archive"></i>&nbsp;
+                            A dropbox link
+                        </p>
+                    </a>
+                </div>
+            </div>
+            <div class="ln-files-wrapper">
+                <div>
+                    <a href="">
+                        
+                        <p>
+                            <i class="fas fa-file-archive"></i>&nbsp;
+                            An one drive link
+                        </p>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
