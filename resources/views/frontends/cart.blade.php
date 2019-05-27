a7bd77161faab962516fa0fbe3a2496219cc6ba8
DuongNT <duongntdev@outlook.com.vn>
2019-05-27 17:15:24 +0700

    css cart

--- resources/views/frontends/cart.blade.php
+++ resources/views/frontends/cart.blade.php
@@ -1,39 +1,35 @@
 @extends('frontends.layouts.app')
 @section('content')
 <div class="cart-page-body background-page">
-    <div class="cart-page-title jumbotron">
-        <h2><i class="fas fa-shopping-cart"></i> Shopping cart</h2>
-    </div>
-    <div class="cart-page-content container">
+    <div class="cart-page-title container">
+        {{-- <img src="http://courdemy.local/frontend/images/tab_cart.png" alt="" style="width: auto;"> --}}
         <div class="cart-pre-info">
             <div class="cart-pre-info-left">
-                <span class="fa-stack">
-                        <i class="fas fa-circle fa-stack-2x" style="color: rgb(200, 201, 202);"></i>
-                        <i class="fas fa-check fa-stack-1x" style="color: rgb(200, 201, 202)"></i>                         
-                </span>
-                <span>
-                    2 courses
-                </span>
-                <span>
-                    100.000 đ
-                </span>
+                <div>
+                    <h2> Shopping cart</h2>
+                </div>
+                <h4>
+                   <i>2 courses in cart</i> 
+                </h4>
             </div>
         </div>
+        <div class="blue-half-square" style=""></div>
+    </div>
+    <div class="cart-page-content container">
+        
         <div class="row">
             <div class="cart-item-list col-md-9">
                 @for ($i = 0; $i < 4; $i++)
                 <div class="cart-single-item">
                     <div class="image">
-                        <img src="frontend/images/course_6.jpg" width="110rem" alt="">
+                        <img src="frontend/images/course_6.jpg" width="130rem" alt="">
                     </div>
                     <div class="course-info">
                         <div class="course-name">Learn Canva from an Expert Designer: Let's Create a Brand!</div>
                         <div class="lecturer-info">By Lindsay Marsh, 14+ Years | Graphic Design:Photoshop, Illustrator, Canva, XD</div>
                     </div>
                     <div class="actions">
-                        <div class="save">Remove</div>
-                        <div class="remove">Save for later</div>
-                        <div class="wishlist">Move to wishlist</div>
+                        <div class="btn-remove"><i class="far fa-trash-alt"></i></div>
                     </div>
                     <div class="single-price">
                         <div>
@@ -52,12 +48,21 @@
             <div class="checkout-column col-md-3">
                 <div>
                     <div class="price-group">
-                        <h2>120 000 ₫</h2>
-                        <h5>199.99 ₫</h5>
-                        <h5>50% off</h5>
-                        <button id="btnCartCheckOut" class="btn btn-danger">Checkout</button>
+                        
+                        <div class="current-price">
+                            <span>120 000 ₫</span>
+                        </div>
+                        <div class="initial-price">
+                            <span>199.99 ₫</span> 
+                        </div>
+                        <div class="percent-off">
+                            <span>50% off</span>
+                        </div>
+                        <div class="btn-checkout">
+                            <button id="btnCartCheckOut" class="btn btn-danger">Checkout</button>
+                        </div>
                     </div>
-                    <div>
+                    <div class="coupon-code-input">
                         <div class="input-group">
                             <input type="text" class="form-control" placeholder="Apply coupon code" aria-describedby="btnCartCouponApply">
                             <span class="input-group-addon" id="btnCartCouponApply">APPLY</span>
