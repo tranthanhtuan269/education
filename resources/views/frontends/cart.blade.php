e0eed346cb4cafbaedd943840aa40583d4080ac9
DuongNT <duongntdev@outlook.com.vn>
2019-05-27 15:17:10 +0700

    s

--- resources/views/frontends/cart.blade.php
+++ resources/views/frontends/cart.blade.php
@@ -20,14 +20,49 @@
             </div>
         </div>
         <div class="row">
-            <div class="cart-item-list col-md-8">
-                <div>
-                
+            <div class="cart-item-list col-md-9">
+                @for ($i = 0; $i < 4; $i++)
+                <div class="cart-single-item">
+                    <div class="image">
+                        <img src="frontend/images/course_6.jpg" width="110rem" alt="">
+                    </div>
+                    <div class="course-info">
+                        <div class="course-name">Learn Canva from an Expert Designer: Let's Create a Brand!</div>
+                        <div class="lecturer-info">By Lindsay Marsh, 14+ Years | Graphic Design:Photoshop, Illustrator, Canva, XD</div>
+                    </div>
+                    <div class="actions">
+                        <div class="save">Remove</div>
+                        <div class="remove">Save for later</div>
+                        <div class="wishlist">Move to wishlist</div>
+                    </div>
+                    <div class="single-price">
+                        <div>
+                            <div>
+                                <div class="current-price">120 000 ₫</div>
+                                <div class="initial-price">199.99 ₫</div>
+                            </div>
+                            <i class="fas fa-tag"></i>
+                        </div>
+                    </div>
                 </div>
+                    
+                @endfor
+                    
             </div>
-            <div class="col-md-4">
+            <div class="checkout-column col-md-3">
                 <div>
-
+                    <div class="price-group">
+                        <h2>120 000 ₫</h2>
+                        <h5>199.99 ₫</h5>
+                        <h5>50% off</h5>
+                        <button id="btnCartCheckOut" class="btn btn-danger">Checkout</button>
+                    </div>
+                    <div>
+                        <div class="input-group">
+                            <input type="text" class="form-control" placeholder="Apply coupon code" aria-describedby="btnCartCouponApply">
+                            <span class="input-group-addon" id="btnCartCouponApply">APPLY</span>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
