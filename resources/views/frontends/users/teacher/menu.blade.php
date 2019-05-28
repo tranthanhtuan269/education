077da5c1b279e44fe49d51ca9b298e64bdaaec56
DESKTOP-8ER9M83\TOH-Dev <tranvanba1992@gmail.com>
2019-05-28 15:57:24 +0700

    php

--- resources/views/frontends/users/teacher/menu.blade.php
+++ resources/views/frontends/users/teacher/menu.blade.php
@@ -10,8 +10,8 @@
     <ul class="nav nav-tabs" style="border-bottom: 0px !important">
         <li class="@if(Request::is('user/teacher/course')) active  @endif"><a href="{{ url('user/teacher/course') }}" ><i class="fa fa-book" aria-hidden="true"></i> Course</a></li>
         <li class="@if(Request::is('user/teacher/profile')) active  @endif"><a href="{{ url('user/teacher/profile') }}" class=""><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
-        <li class="hidden-sm "><a href="{{ url('coming-soon') }}" class=""><i class="fa fa-search-plus" aria-hidden="true"></i> Discover</a></li>
-        <li class="hiden-md hidden-sm "><a href="{{ url('coming-soon') }}" class=""><i class="fa fa-gift" aria-hidden="true"></i> Gift</a></li>
+        {{-- <li class="hidden-sm "><a href="{{ url('coming-soon') }}" class=""><i class="fa fa-search-plus" aria-hidden="true"></i> Discover</a></li>
+        <li class="hiden-md hidden-sm "><a href="{{ url('coming-soon') }}" class=""><i class="fa fa-gift" aria-hidden="true"></i> Gift</a></li> --}}
         {{-- <li class="hiden-md hidden-sm "><a href="{{ url('coming-soon') }}" class=""><i class="far fa-money-bill-alt"></i> Recharge</a></li> --}}
         <li class="hidden-md hidden-sm ">
             <a href="{{ url('coming-soon') }}" class=""> <i class="fa fa-history" aria-hidden="true"></i> Order history</a>
