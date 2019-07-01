a8b3773a3959e5f9b44b0c8a2660888252ffebc8
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-07-01 10:44:48 +0700

    Fix Width modal Sign up and Change Password

--- resources/views/frontends/layouts/app.blade.php
+++ resources/views/frontends/layouts/app.blade.php
@@ -424,7 +424,7 @@
                                         </div>
                                     </div>
                                     <div id="myModalRegister" class="modal fade" role="dialog" >
-                                        <div class="modal-dialog">
+                                        <div class="modal-dialog modal-login">
                                             <div class="modal-content">
                                                 <div class="modal-header">				
                                                     <button type="button" class="close" data-dismiss="modal">&times;</button>
