68083429db6ac9cf5f93b149bc4d5309283aa3e6
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-07-09 14:13:10 +0700

    check popular teacher

--- resources/views/frontends/popular-teacher.blade.php
+++ resources/views/frontends/popular-teacher.blade.php
@@ -5,6 +5,7 @@
                 <h2>Popular Teacher</h2>
             </div>
             @foreach ($popular_teacher as $teacher)
+            @if($teacher->status)
                 @include(
                     'components.teacher', 
                     [
@@ -18,6 +19,7 @@
                         'student_number' => $teacher->student_count
                     ]
                 )
+            @endif
             @endforeach
         </div>
     </div>
