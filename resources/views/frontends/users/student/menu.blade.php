43dfeb25c793411614b192a968f234fef9a6596d
tuantt <tuantt@tohsoft.com>
2019-07-16 08:14:40 +0700

    remove some tabs of admin

--- resources/views/frontends/users/student/menu.blade.php
+++ resources/views/frontends/users/student/menu.blade.php
@@ -12,6 +12,7 @@
 </div>
 <div class="collapse navbar-collapse" id="myNavbar">
     <ul class="nav nav-tabs" style="border-bottom: 0px !important">
+        @if(Auth::user()->userRoles()->where('role_id',1)->count() <= 0)
         <li class="@if(Request::is('user/student/course')) active  @endif"><a href="{{ url('user/student/course') }}" ><i class="fa fa-book" aria-hidden="true"></i> Course</a></li>
         <li class="@if(Request::is('user/student/profile')) active  @endif"><a href="{{ url('user/student/profile') }}" class=""><i class="fa fa-user" aria-hidden="true"></i> Profile</a></li>
         {{-- <li class="hidden-sm "><a href="{{ url('coming-soon') }}" class=""><i class="fa fa-search-plus" aria-hidden="true"></i> Discover</a></li>
@@ -23,5 +24,10 @@
         <li class="@if(Request::is('user/student/mail-box')) active  @endif">
             <a href="{{ url('user/student/mail-box') }}" class=""> <i class="fas fa-envelope"></i> Mailbox</a>
         </li>
+        @else
+        <li class="@if(Request::is('user/student/mail-box')) active  @endif">
+            <a href="{{ url('user/student/mail-box') }}" class=""> <i class="fas fa-envelope"></i> Mailbox</a>
+        </li>
+        @endif
     </ul>
 </div>
