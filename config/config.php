bddf88e16a5442a098ecd45d49c11a6fdacf7cb6
DESKTOP-8ER9M83\TOH-Dev <tranvanba1992@gmail.com>
2019-07-19 15:55:46 +0700

    aa

--- config/config.php
+++ config/config.php
@@ -1,9 +1,9 @@
 <?php
 	return array(
-		'path_ffmpeg_exe' => '"C:\ffmpeg\bin\ffmpeg.exe"',
-		'path_ffprobe_exe' => '"C:\ffmpeg\bin\ffprobe.exe"',
+		// 'path_ffmpeg_exe' => '"C:\ffmpeg\bin\ffmpeg.exe"',
+		// 'path_ffprobe_exe' => '"C:\ffmpeg\bin\ffprobe.exe"',
 
-		// 'path_ffmpeg_exe' => 'ffmpeg',
-		// 'path_ffprobe_exe' => 'ffprobe'
+		'path_ffmpeg_exe' => 'ffmpeg',
+		'path_ffprobe_exe' => 'ffprobe'
 	);
 
