c8d4a48e2447038e895dc154fd21541867d97147
gnoud <duongntdev@outlook.com.vn>
2019-08-08 08:26:52 +0700

    hot fix

--- resources/views/frontends/feature-courses.blade.php
+++ resources/views/frontends/feature-courses.blade.php
@@ -9,12 +9,20 @@
             @if (isset($feature_course[0]))
             <div class="col-sm-7 mt-xs-120px">
                 <a href="{{ url('/') }}/course/{{ $feature_course[0]->slug }}">
+                    @if (strpos($feature_course[0]->image, 'unica') !== false)
+                    <div class="top-feature-course" style="background-image:url({{ $feature_course[0]->image }}); width: 100%; height:390">
+                    @else
                     <div class="top-feature-course" style="background-image:url({{ asset('frontend/images/'.$feature_course[0]->image) }}); width: 100%; height:390">
+                    @endif
                         <div class="course-info">
                         <h3>{{ $feature_course[0]->name }}</h3>
                             <div class="course-detail">
                                 <div class="course-info-author">
+                                    @if (strpos($feature_course[0]->Lecturers()[0]->user->avatar, 'unica') !== false)
+                                    <img src="{{ $feature_course[0]->Lecturers()[0]->user->avatar }}" alt="">
+                                    @else
                                     <img src="{{url('frontend/'.$feature_course[0]->Lecturers()[0]->user->avatar)}}" alt="">
+                                    @endif
                                     <span class="info-author-name">Giảng viên:<b> {{$feature_course[0]->Lecturers()[0]->user->name}}</b></span>
                                 </div>
                                 
@@ -38,7 +46,11 @@
                         <!-- <a href="{{ url('/') }}/course/{{ $feature_course[1]->slug }}" title="{{ $feature_course[1]->name }}">
                             <img  alt="{{ $feature_course[1]->name }}" src="{{ asset('frontend/images/'.$feature_course[1]->image) }}" height="187">
                         </a> -->
+                        @if (strpos($feature_course[1]->image, 'unica') !== false)
+                        <div class="top-feature-course" style="background-image:url({{ $feature_course[1]->image }}); width: 100%; height:187">                            
+                        @else
                         <div class="top-feature-course" style="background-image:url({{ asset('frontend/images/'.$feature_course[1]->image) }}); width: 100%; height:187">
+                        @endif
                             <div class="course-info">
                                 <h5>{{ $feature_course[1]->name }}</h5>
                             </div>
@@ -53,7 +65,11 @@
                             <!-- <a href="{{ url('/') }}/course/{{ $feature_course[2]->slug }}" title="{{ $feature_course[2]->name }}">
                                 <img  alt="{{ $feature_course[2]->name }}" src="{{ asset('frontend/images/'.$feature_course[2]->image) }}" height="187">
                             </a> -->
+                        @if (strpos($feature_course[1]->image, 'unica') !== false)
+                        <div class="top-feature-course" style="background-image:url({{ $feature_course[2]->image }}); width: 100%; height:187">                            
+                        @else
                         <div class="top-feature-course" style="background-image:url({{ asset('frontend/images/'.$feature_course[2]->image) }}); width: 100%; height:187">
+                        @endif
                         <div class="course-info">
                             <h5>{{ $feature_course[2]->name }}</h5>
                         </div>
