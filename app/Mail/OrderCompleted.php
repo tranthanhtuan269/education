6fd032fdfb88ae68fddd7e2ee5f46445573ecf19
gnoud <duongntdev@outlook.com.vn>
2019-08-21 10:37:50 +0700

    email

--- app/Mail/OrderCompleted.php
+++ app/Mail/OrderCompleted.php
@@ -6,25 +6,26 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
+use App\Order;
 use App\User;
-use App\Email;
+use App\Course;
 
-class DiscountNot extends Mailable
+class OrderCompleted extends Mailable
 {
     use Queueable, SerializesModels;
-    
 
     protected $user;
-    protected $email;
+    protected $order;
+
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct(User $user, Email $email )
+    public function __construct(Order $order, User $user)
     {
+        $this->order = $order;
         $this->user = $user;
-        $this->email = $email;
     }
 
     /**
@@ -35,12 +36,14 @@ class DiscountNot extends Mailable
     public function build()
     {
         return $this->from('duongconuong@tohsoft.com')
-                    ->view('backends.emails.discount-not')
-                    ->subject($this->email->title)
+                    ->view('frontends.emails.order-completed-not')
+                    ->subject('Order Completed')
                     ->with([
-                        'userName' => $this->user->name,
-                        'mailSubject' => $this->email->title,
-                        'mailContent' => $this->email->content,
+                        'user' => $this->user,
+                        'order' => $this->order,
+                        'courses' => $this->order->courses,
+                        'mailSubject' => 'Order Completed',
+                        'mailContent' => 'Alibaba',
                     ]);
     }
 }
