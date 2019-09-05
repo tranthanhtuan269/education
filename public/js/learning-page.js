b78ec739cfbec996c7313dd88b6ae1d9d082f492
gnoud <duongntdev@outlook.com.vn>
2019-09-05 14:13:38 +0700

    f

--- public/js/learning-page.js
+++ public/js/learning-page.js
@@ -1,5 +1,8 @@
 const baseURL = $('base').attr('href');
 
+
+
+
 let isAutoplay;
 if(localStorage.getItem('autoplay') != null){
     isAutoplay = localStorage.getItem('autoplay')
@@ -8,7 +11,16 @@ if(localStorage.getItem('autoplay') != null){
     localStorage.setItem('autoplay', newLocal)
     isAutoplay = localStorage.getItem('autoplay')
 }
+
 $(document).ready(function () {
+    //Check browser có phải là firefox hay không để hiện thông báo
+    var isFirefox = typeof InstallTrigger !== 'undefined';
+    if(isFirefox && isAutoplay){
+        Swal.fire({
+            type : 'info',
+            html: 'Bạn đang bật tính năng tự động chạy bài giảng. <br> Từ phiên bản Firefox 66, Firefox đã ngừng hỗ trợ theo mặc định tính năng tự động chạy video/audio. Để bật tính năng này cho trình duyệt Firefox của bạn, vui lòng làm theo hướng dẫn tại <a href="https://support.mozilla.org/vi/kb/block-autoplay">đây.</a>'
+        })
+    }
 
     // Set up the player
     var isPlayerAutoplay = false
