fe2474491c5edba2af26f749d86592a9f1d49c85
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-09-06 16:19:51 +0700

    #58

--- app/Http/Controllers/Frontends/HomeController.php
+++ app/Http/Controllers/Frontends/HomeController.php
@@ -778,21 +778,21 @@ class HomeController extends Controller
         // $course = Course::find(1);
         // dd($course->userRoles);
 
-        // $teachers = Teacher::get();
-        // foreach($teachers as $teacher){
-        //     $teacher->student_count = 0;
-        //     $teacher->save();
-        // }
+        $teachers = Teacher::get();
+        foreach($teachers as $teacher){
+            $teacher->student_count = 0;
+            $teacher->save();
+        }
 
-        // foreach($courses as $course){
-        //     if($course->Lecturers()->first() && $course->Lecturers()->first()->teacher){
-        //         $teacher = Teacher::find($course->Lecturers()->first()->teacher->id);
-        //         if($teacher){
-        //             $teacher->student_count += $course->student_count;
-        //             $teacher->save();
-        //         }
-        //     }
-        // }
+        foreach($courses as $course){
+            if($course->Lecturers()->first() && $course->Lecturers()->first()->teacher){
+                $teacher = Teacher::find($course->Lecturers()->first()->teacher->id);
+                if($teacher){
+                    $teacher->student_count += $course->student_count;
+                    $teacher->save();
+                }
+            }
+        }
         echo "done";
     }
 
