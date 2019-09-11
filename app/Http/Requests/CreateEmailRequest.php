31f35f00151c11fb6bcfad902af19e7c5180f035
C <congnd@tohsoft.com>
2019-09-11 09:59:19 +0700

    200

---
+++ app/Http/Requests/CreateEmailRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Auth;
+
+class CreateEmailRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return Auth::check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'title' => 'required',
+            'content' => 'required'
+        ];
+    }
+    public function messages() {
+        return [
+            'title.required' => "Bạn chưa điền chủ đề của Email",
+            'content.required' => "Bạn chưa điền nội dung của Email",
+        ];
+    }
+}
