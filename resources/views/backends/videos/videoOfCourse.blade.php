83e4f4430a9766a8b39a9bfdb70442f2c16a2a2d
tuantt <tuantt@tohsoft.com>
2019-10-08 20:40:01 +0700

    css

--- resources/views/backends/videos/videoOfCourse.blade.php
+++ resources/views/backends/videos/videoOfCourse.blade.php
@@ -114,9 +114,9 @@
                     var html = '';
                     @if (Helper::checkPermissions('videos.accept-video', $list_roles)) 
                         if(row['state'] == 0){
-                            html += '<a class="btn-accept mr-2 inaccept-video" data-id="'+data+'" data-title="'+row.title+'" data-content="'+row.content+'" title="Hủy"> <i class="fa fa-check fa-fw"></i></a>';
+                            html += '<a class="btn-accept mr-2 inaccept-video" data-id="'+data+'" data-title="'+row.title+'" data-content="'+row.content+'" title="Duyệt"> <i class="fa fa-check fa-fw"></i></a>';
                         }else{
-                            html += '<a class="btn-inaccept mr-2 accept-video" data-id="'+data+'" data-title="'+row.title+'" data-content="'+row.content+'" title="Duyệt"> <i class="fa fa-times fa-fw"></i></a>';
+                            html += '<a class="btn-inaccept mr-2 accept-video" data-id="'+data+'" data-title="'+row.title+'" data-content="'+row.content+'" title="Hủy"> <i class="fa fa-times fa-fw"></i></a>';
                         }
                     @endif
                     return html;
