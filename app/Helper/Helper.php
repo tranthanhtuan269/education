85bd5f63fb50265b21f13d1be2bd103a8da165e9
tuantt <tuantt@tohsoft.com>
2019-10-10 10:03:55 +0700

    css

--- app/Helper/Helper.php
+++ app/Helper/Helper.php
@@ -18,7 +18,7 @@ class Helper
         if(Auth::check()){
             $user_id = Auth::user()->id;
             if(Auth::user()->isAdmin()){
-                return Auth::user()->isAdmin();
+                return Auth::user()->userRolesAdmin();
             }
             $user_role_list = Auth::user()->userRoles;
             $course = \App\Course::find($course_id);
