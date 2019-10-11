6f2a8fdfb0649438a4ed9d6604303d4c5322a7a0
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-10-11 16:28:26 +0700

    Ticket #87[Play courses]: Không lưu và xem được thảo luận ghi chú khi nhập vào đoạn text dài

--- resources/views/frontends/learning-page/discuss.blade.php
+++ resources/views/frontends/learning-page/discuss.blade.php
@@ -225,6 +225,18 @@
                     parentId : parentId,
                 },
                 dataType: "json",
+                error: function (error) {
+                    var obj_errors = error.responseJSON.errors;
+                    var txt_errors = '';
+                    for (k of Object.keys(obj_errors)) {
+                        txt_errors += obj_errors[k][0] + '</br>';
+                    }
+                    Swal.fire({
+                        type: 'warning',
+                        html: txt_errors,
+                        allowOutsideClick: false,
+                    })
+                }
             })
 
             request.done(function( response ) {
