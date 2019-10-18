0768fb2b52b1ccaae592cbcdb31d566b4016942a
tuantt <tuantt@tohsoft.com>
2019-10-18 19:02:00 +0700

    ss

--- app/Providers/AppServiceProvider2.php
+++ app/Providers/AppServiceProvider2.php
@@ -110,6 +110,8 @@ class AppServiceProvider2 extends ServiceProvider
                     $video->state = \Config::get('app.video_active');
                     $video->save();
                 }
+
+                Helper::reSortIndexVideoOfCourse($video->unit->course->id);
                 
                 $json_data = \json_encode($data->video_id);
             }
