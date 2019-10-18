f83ddb801700167b5b075a6244f813ee8e47a5ad
tuantt <tuantt@tohsoft.com>
2019-10-18 10:51:51 +0700

    css

--- config/app.php
+++ config/app.php
@@ -192,7 +192,7 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider2::class,
+        App\Providers\AppServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
