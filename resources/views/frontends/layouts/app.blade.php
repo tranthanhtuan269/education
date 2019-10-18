0e0f3ce2b940094cf84a91379e9ea680135ebb4d
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-10-18 09:17:00 +0700

    version

--- resources/views/frontends/layouts/app.blade.php
+++ resources/views/frontends/layouts/app.blade.php
@@ -452,7 +452,7 @@
                             <li><a href="javascript:void(0)" title="job post">Job Post</a></li>
                         </ul> --}}
                         <p class="copyright">&copy; 2019, Bản quyền thuộc về courdemy.com. Bảo lưu mọi quyền!</p>
-                        <p>v2019.10.15</p>
+                        <p>v2019.10.17</p>
                     </div>
                 </div>
             </div>
