d3ca2b19ffa6f586d6a29e1bd29f41236bece911
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-11-13 13:59:08 +0700

    fb

--- resources/views/components/facebook-login.blade.php
+++ resources/views/components/facebook-login.blade.php
@@ -52,10 +52,10 @@
     function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
         console.log('Welcome!  Fetching your information.... ');
         FB.api('/me', function(response) {
-            console.log('Successful login for: ' + response.name);
+            // console.log('Successful login for: ' + response.name);
             console.log(response)
-            document.getElementById('status').innerHTML =
-                'Cảm ơn bạn đã đăng nhập, ' + response.name + '!' + response.email;
+            // document.getElementById('status').innerHTML =
+                // 'Cảm ơn bạn đã đăng nhập, ' + response.name + '!' + response.email;
             $.ajaxSetup({
                 headers: {
                     'X-CSRF-TOKEN'    : $('meta[name="csrf-token"]').attr('content')
