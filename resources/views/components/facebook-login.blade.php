aa8edae73b875574e41d2748c21b18d03f8633b3
trinhnk <nguyenkimtrinh1993@gmail.com>
2019-11-13 14:41:23 +0700

    fb

--- resources/views/components/facebook-login.blade.php
+++ resources/views/components/facebook-login.blade.php
@@ -11,11 +11,9 @@
 <div id="status"></div>
 
 <script>
-    var facebook_name = ''
-    var facebook_id = ''
     function statusChangeCallback(response) {  // Called with the results from FB.getLoginStatus().
-        // console.log('statusChangeCallback');
-        // console.log(response);                   // The current login status of the person.
+        console.log('statusChangeCallback');
+        console.log(response);                   // The current login status of the person.
         if (response.status === 'connected') {   // Logged into your webpage and Facebook.
             testAPI();
         } else {                                 // Not logged into your webpage or we are unable to tell.
@@ -27,57 +25,6 @@
     function checkLoginState() {               // Called when a person is finished with the Login Button.
         FB.getLoginStatus(function(response) {   // See the onlogin handler
             statusChangeCallback(response);
-        });
-
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN'    : $('meta[name="csrf-token"]').attr('content')
-            }
-        });
-        $.ajax({
-            url: "/facebookLogin",
-            data: {
-                name        : facebook_name,
-                facebook_id : facebook_id,
-            },
-            method: "POST",
-            dataType:'json',
-            beforeSend: function(r, a){
-                $('.alert-errors').addClass('d-none');
-            },
-            success: function (response) {
-                if(response.status == 200){
-                    Swal.fire({
-                        type: 'success',
-                        text: 'Đăng nhập thành công!'
-                    }).then(result => {
-                        location.reload()
-                    })
-                } else {
-                    if(response.status == 201){
-                        Swal.fire({
-                            type: 'success',
-                            text: 'Đăng ký tài khoản thành công!'
-                        }).then(result => {
-                            location.reload()
-                        })
-                    }else{
-                        Swal.fire({
-                            type: 'warning',
-                            text: 'Đăng nhập thất bại'
-                        })
-                    }
-                }
-            },
-            error: function (error) {
-                var obj_errors = error;
-                console.log(obj_errors)
-                var txt_errors = 'Lỗi';
-                Swal.fire({
-                    type: 'warning',
-                    html: txt_errors,
-                })
-            }
         });
     }
 
@@ -103,14 +50,64 @@
     }(document, 'script', 'facebook-jssdk'));
 
     function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
-        // console.log('Welcome!  Fetching your information.... ');
+        console.log('Welcome!  Fetching your information.... ');
         FB.api('/me', function(response) {
             // console.log('Successful login for: ' + response.name);
             console.log(response)
             // document.getElementById('status').innerHTML =
                 // 'Cảm ơn bạn đã đăng nhập, ' + response.name + '!' + response.email;
-            facebook_name   = response.name
-            facebook_id     = response.id
+            var facebook_id = response.name;
+            var facebook_name = response.id;
+            $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN'    : $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+            $.ajax({
+                url: "/facebookLogin",
+                data: {
+                    name        : facebook_id,
+                    facebook_id : facebook_name,
+                },
+                method: "POST",
+                dataType:'json',
+                beforeSend: function(r, a){
+                    $('.alert-errors').addClass('d-none');
+                },
+                success: function (response) {
+                    if(response.status == 200){
+                        Swal.fire({
+                            type: 'success',
+                            text: 'Đăng nhập thành công!'
+                        }).then(result => {
+                            location.reload()
+                        })
+                    } else {
+                        if(response.status == 201){
+                            Swal.fire({
+                                type: 'success',
+                                text: 'Đăng ký tài khoản thành công!'
+                            }).then(result => {
+                                location.reload()
+                            })
+                        }else{
+                            Swal.fire({
+                                type: 'warning',
+                                text: 'Đăng nhập thất bại'
+                            })
+                        }
+                    }
+                },
+                error: function (error) {
+                    var obj_errors = error;
+                    console.log(obj_errors)
+                    var txt_errors = 'Lỗi';
+                    Swal.fire({
+                        type: 'warning',
+                        html: txt_errors,
+                    })
+                }
+            });
         });
     }
 
