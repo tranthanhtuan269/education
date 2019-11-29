3206f61203ee3080e1839c01ea331f99e298a1ce
DuongNT <tungduong284@gmail.com>
2019-11-29 09:27:59 +0700

    a

--- resources/views/frontends/learning-page/player.blade.php
+++ resources/views/frontends/learning-page/player.blade.php
@@ -1,7 +1,7 @@
 <div class="learning-video" style="overflow-y:hidden;overflow-x:hidden">
 	<div class="btn-showhide btn-show btn-video"><i class="fa fa-angle-double-left" aria-hidden="true"></i></div>
     <div class="btn-showhide btn-hide btn-video"><i class="fa fa-angle-double-right" aria-hidden="true"></i></div>
-    <video id='my-video' class='video-js vjs-big-play-centered leftBarActive' src=""> 
+    <video id='my-video' class='video-js vjs-big-play-centered leftBarActive' src="" muted="muted"> 
         <p class='vjs-no-js'>
             To view this video please enable JavaScript, and consider upgrading to a web browser that
             <a href='https://videojs.com/html5-video-support/' target='_blank'>supports HTML5 video</a>
